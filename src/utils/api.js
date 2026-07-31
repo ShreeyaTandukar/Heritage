c8cd25ed66789a6e464925b24a4df56855820340
ShreeyaTandukar <tandukar.shreeya56@gmail.com>
2026-07-31 20:30:31 +0545

    modified api  file

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Single source of truth for the backend URL.
 // Change this in one place if the backend moves off localhost:5000.
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://192.168.100.1/api",
 });
 
 // Automatically attach the JWT (if we have one) to every request.
