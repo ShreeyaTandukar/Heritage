5b40234b5759735105891c9ec81cdbc96d253f46
ShreeyaTandukar <tandukar.shreeya56@gmail.com>
2026-09-09 23:25:50 +0545

    fix MongoDB DNS connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected")
-    } catch(error){
-        console.error("MongoDB Connection Failed");
-        console.error(error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB Connection Failed");
+    console.error(error.message);
+    process.exit(1);
+  }
 };
+
 module.exports = connectDB;
